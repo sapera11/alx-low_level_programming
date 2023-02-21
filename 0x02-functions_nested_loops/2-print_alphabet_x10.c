542019c03f9dfd5f292459a505eaee9da75dd3e4
akinseteWilliams <akinsetewilliams@gmail.com>
2023-02-21 15:58:33 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <=9)
+	while (i <= 9)
 	{
-	for ( c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
 	i++;
-	}	
+	}
 }
-
-
