6e0812c24795cb04b76121c6a98c97d4eb9e665f
akinseteWilliams <akinsetewilliams@gmail.com>
2023-02-22 10:36:21 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * jack_baver - Prints every minute of the day of Jack Bauer
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return: ...
  */
 
-void jack_baver(void)
+void jack_bauer(void)
 
 {
 	int a, b, c, d;
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    jack_baver();
+    jack_bauer();
     return (0);
 }
